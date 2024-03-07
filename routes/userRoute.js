c99814cf8f3e36509b6068f225667c2eaa6e3ff3
Hemnaath <hemnaathrsurya@gmail.com>
2024-03-07 14:36:55 +0530

    node modules

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const userController = require('../controller/userController');
-const authenticateUser = require('../midleware/auth');
+const authenticateUser = require('../middleware/auth');
 
 const router = express.Router();
 
@@ -8,7 +8,7 @@ router.post('/createUser', userController.createUser);
 router.post('/signUp', userController.signUp);
 router.get('/signIn', userController.signIn);
 router.put('/updateUser/:id', userController.updateUser);
-router.delete('/deleteUser/:id', userController.deleteUser);
+router.delete('/deleteUser/:id', userController.deleteUser); 
 router.get('/getAllUser', authenticateUser, userController.getAllUser);
 router.get('/getUserById/:id', userController.getUserById);
 
