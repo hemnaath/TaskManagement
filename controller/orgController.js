64a8702632e6ccd8f2c786b239a8816384877a2c
Hemnaath <hemnaathrsurya@gmail.com>
2024-04-17 00:34:59 +0530

    org allowed duplicates and email

--- controller/orgController.js
+++ controller/orgController.js
@@ -2,21 +2,14 @@ const db = require('../config/sqlConnector');
 
 const createOrg = (req, res) => {
     const { orgName, orgType } = req.body;
-    const findOrgSql = `SELECT id FROM org WHERE orgName = ?`;
-    db.query(findOrgSql, [orgName], (err, value) => {
-        if (value.length > 0) {
-            return res.status(400).json({ message: 'Org Exists' });
-        } else {
-            const createOrgSql = `INSERT INTO org (orgName, orgType) VALUES (?, ?)`;
-            db.query(createOrgSql, [orgName, orgType], (err, values) => {
-                const orgId = values.insertId;
-                const userId = res.locals.id;
-                const updater = `UPDATE user SET orgId = ? WHERE id = ?`;
-                db.query(updater, [orgId, userId], (err, updaterResult) => {
-                    return res.status(201).json({ message: 'Org Created' });
-                });
-            });
-        }
+    const createOrgSql = `INSERT INTO org (orgName, orgType) VALUES (?, ?)`;
+    db.query(createOrgSql, [orgName, orgType], (err, values) => {
+        const orgId = values.insertId;
+        const userId = res.locals.id;
+        const updater = `UPDATE user SET orgId = ? WHERE id = ?`;
+        db.query(updater, [orgId, userId], (err, updaterResult) => {
+            return res.status(201).json({ message: 'Org Created' });
+        });
     });
 }
 
