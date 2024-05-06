c45fddc3b53d7fc5394808f428b2e353f449f49f
Hemnaath <hemnaathrsurya@gmail.com>
2024-05-06 09:04:56 +0530

    approve leave

--- controller/leaveController.js
+++ controller/leaveController.js
@@ -12,7 +12,7 @@ const resetLeave = async(req, res)=>{
 }
 
 const applyLeave = async(req, res)=>{
-    const {leave_type, applied_on, reason, emergency_contact} = req.body;
+    const {leave_type, applying_on, reason, emergency_contact} = req.body;
     try{
         const finder = await User.findOne({_id:res.locals.id});
         if (finder) {
@@ -20,7 +20,7 @@ const applyLeave = async(req, res)=>{
             for(let key in finderObj){
                 if(key === leave_type){
                     if(finderObj[key] >=1){
-                        const apply = await Leave.create({leave_type, applied_on, status:'pending', reason, emergency_contact, applied_by:res.locals.id});
+                        const apply = await Leave.create({leave_type, applying_on, status:'pending', reason, emergency_contact, applied_by:res.locals.id});
                         return res.status(200).json({message:'Leave applied', apply});
                     }else{
                         return res.status(400).json('No more' + ' ' + `${leave_type}`);
@@ -35,37 +35,47 @@ const applyLeave = async(req, res)=>{
     }
 }
 
+const leaveRequest = async (req, res) => {
+    try {
+        const downLine = await User.find({ reporting_person: res.locals.id });
+        const leaveRequests = await Leave.find({ applied_by: { $in: downLine.map(user => user.id) } });
+        if(leaveRequests){
+            return res.status(200).json(leaveRequests);
+        }
+        return res.status(404).json('No Leave requests found');
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json('Internal Server Error');
+    }
+}
+
 const approveLeave = async(req, res)=>{
     const leaveId = req.params.id;
-    const {status} = req.body;
     try{
-        let approvalFlag = false;
-        if(status === 'approved' || status === 'Approved'){
-            approvalFlag = true;
-        }
         const exists = await Leave.findOne({_id:leaveId});
-        if(exists && approvalFlag){
+        if(exists){
             const userId = exists.applied_by;
-            const leave_type = exists.leave_type;
-            const userExists = await User.findOne({_id:userId})
+            const leaveType = exists.leave_type;
+            exists.status = 'approved';
+            await exists.save();
+            const userExists = await User.findOne({_id:userId});
             if(userExists){
-                userExists[leave_type] -=1;
+                userExists[leaveType] -= 1;
                 await userExists.save();
             }
-            exists[status] = status;
-            await exists.save();
-            return res.status(200).json('Leave approved');
-        }else{
-            return res.status(404).json('Leave does not exists');
+            return res.status(301).json('Leave Approved');
         }
+        return res.status(404).json('User not Found');
     }catch(error){
         return res.status(500).json('Internal Server Error');
     }
 }
 
 
+
 module.exports = {
     resetLeave,
     applyLeave,
+    leaveRequest,
     approveLeave,
 }
--- routes/leaveRoute.js
+++ routes/leaveRoute.js
@@ -8,8 +8,10 @@ router.post('/reset', authenticateUser, (req, res, next)=>{
     if(res.locals.role !== 'admin'){
         return res.status(403).json('Unauthorized Access');
     }
+    next();
 }, leaveController.resetLeave);
 router.post('/apply-leave', authenticateUser, leaveController.applyLeave);
+router.get('/leave-request', authenticateUser, leaveController.leaveRequest);
 router.put('/approve-leave/:id', authenticateUser, leaveController.approveLeave);
 
 module.exports = router;
