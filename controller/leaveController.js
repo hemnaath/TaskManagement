fcea3e91af6cb5d4f992633fb63fb81a9d92edfc
Hemnaath <hemnaathrsurya@gmail.com>
2024-05-23 16:21:15 +0530

    leave changes

--- controller/leaveController.js
+++ controller/leaveController.js
@@ -2,9 +2,9 @@ const User = require('../model/userModel');
 const Leave = require('../model/leaveModel');
 
 const resetLeave = async(req, res)=>{
-    const {cl, sl, permission} = req.body;
+    const {casualLeave, sickLeave, permission} = req.body;
     try{
-        await User.updateMany({}, {$set:{casual_leave:cl, sick_leave:sl, permission:permission}});
+        await User.updateMany({}, {$set:{casual_leave:casualLeave, sick_leave:sickLeave, permission:permission}});
         return res.status(200).json({message:'Leave updated'});
     }catch(error){
         console.log(error);
@@ -12,30 +12,33 @@ const resetLeave = async(req, res)=>{
     }
 }
 
-const applyLeave = async(req, res)=>{
-    const {leave_type, applying_on, reason, emergency_contact} = req.body;
-    try{
-        const finder = await User.findById(res.locals.id);
-        if (finder) {
-            const finderObj = finder.toObject();
-            for(let key in finderObj){
-                if(key === leave_type){
-                    if(finderObj[key] >=1){
-                        const apply = await Leave.create({leave_type, applying_on, status:'pending', reason, emergency_contact, applied_by:res.locals.id});
-                        return res.status(200).json({message:'Leave applied', apply});
-                    }else{
-                        return res.status(400).json({message:'No more' + ' ' + `${leave_type}`});
-                    }
-                }
+const applyLeave = async (req, res) => {
+    const { leaveType, startDate, endDate, startTime, endTime, reason, emergencyContact } = req.body;
+    try {
+        const user = await User.findById(res.locals.id);
+        if (user) {
+            const leaveBalance = user[leaveType];
+            if (leaveBalance >= 1) {
+                const start_date = Date.parse(startDate);
+                const end_date = Date.parse(endDate);
+                const start_time = startTime ? Date.parse(startTime) : null;
+                const end_time = endTime ? Date.parse(endTime) : null;
+                const oneDay = 24 * 60 * 60 * 1000;
+                const total_days = Math.round(Math.abs((end_date - start_date) / oneDay)) + 1;
+                const leaveApplication = await Leave.create({leave_type:leaveType, start_date, end_date, start_time, end_time, status:'pending', reason, total_days, emergency_contact:emergencyContact, applied_by:res.locals.id});
+                return res.status(200).json({ message: 'Leave applied', leaveApplication });
+            } else {
+                return res.status(400).json({ message: `No more ${leaveType}` });
             }
-        }else{
-            return res.status(404).json({message:'User not Found'});
+        } else {
+            return res.status(404).json({ message: 'User not found' });
         }
-    }catch(error){
+    } catch (error) {
         console.log(error);
-        return res.status(500).json({error:'Internal Server Error'});
+        return res.status(500).json({ error: 'Internal Server Error' });
     }
-}
+};
+
 
 const leaveRequest = async (req, res) => {
     try {
@@ -62,7 +65,7 @@ const approveLeave = async(req, res)=>{
             await exists.save();
             const userExists = await User.findById(userId);
             if(userExists){
-                userExists[leaveType] -= 1;
+                userExists[leaveType] -= exists.total_days;
                 await userExists.save();
             }
             return res.status(301).json({message:'Leave Approved'});
--- model/leaveModel.js
+++ model/leaveModel.js
@@ -1,17 +1,20 @@
 const mongoose = require('mongoose');
 
 const leaveSchema = new mongoose.Schema({
-    leave_type:{type:String}, 
-    applied_on:{type:String}, 
-    status:{type:String}, 
-    reason:{type:String}, 
-    emergency_contact:{type:Number},
-    applied_by:{type:mongoose.Schema.Types.ObjectId}
-},
-{
-    timestamps:true
+    leave_type: { type: String, enum: ['casual_leave', 'sick_leave', 'permission'], required: true },
+    start_date: { type: Date, required: true },
+    end_date: { type: Date, default: null },
+    start_time: { type: Date, default: null },
+    end_time: { type: Date, default: null },
+    status: { type: String, default: 'pending' },
+    reason: { type: String, required: true },
+    total_days: { type: Number, default: null },
+    emergency_contact: { type: Number, required: true },
+    applied_by: { type: mongoose.Schema.Types.ObjectId, required: true }
+}, {
+    timestamps: true
 });
 
-const leave = mongoose.model('leave', leaveSchema);
+const Leave = mongoose.model('Leave', leaveSchema);
 
-module.exports = leave;
+module.exports = Leave;
