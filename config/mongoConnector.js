aed881b1875510f71b75ed422fa01f7f0e77f011
Hemnaath <hemnaathrsurya@gmail.com>
2024-06-06 17:48:21 +0530

    cloud db changed

--- config/mongoConnector.js
+++ config/mongoConnector.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb+srv://RS-Tech:Insideout%4018!!@cluster0.4u4yuef.mongodb.net/CRM');
+mongoose.connect('mongodb+srv://RS_TECH_DEV:rstechdev@cluster0.4u4yuef.mongodb.net/CRM');
 console.log("Database Connected");
 
 
