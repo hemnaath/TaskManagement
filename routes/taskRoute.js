750b41b61483a1b8e42744de42accd9fef763de8
Hemnaath <hemnaathrsurya@gmail.com>
2024-07-01 15:20:29 +0530

    task route

--- routes/taskRoute.js
+++ routes/taskRoute.js
@@ -14,7 +14,6 @@ const authenticateUser = passport.authenticate('jwt', { session: false });
 router.post('/create-task/:id', authenticateUser, createRateLimiter(10 * 60 * 1000, 50), fileHelper.upload.single('File'), taskController.createTask);
 router.patch('/update-task/:id', authenticateUser, createRateLimiter(10 * 60 * 1000, 50), fileHelper.upload.single('File'), taskController.updateTask);
 router.delete('/delete-task/:id', authenticateUser, createRateLimiter(10 * 60 * 1000, 50), taskController.deleteTask);
-router.get('/task-pagination/:id', authenticateUser, createRateLimiter(10 * 60 * 1000, 50), taskController.taskPagination);
 router.get('/get-task/:id', authenticateUser, createRateLimiter(10 * 60 * 1000, 50), taskController.getTask);
 
 module.exports = router;
