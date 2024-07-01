7d78a2c10cbc534f869cba473c096831fc59c3de
Hemnaath <hemnaathrsurya@gmail.com>
2024-07-01 15:17:32 +0530

    permissions removed

--- routes/projectRoute.js
+++ routes/projectRoute.js
@@ -2,29 +2,17 @@ const express = require('express');
 const projectController = require('../controller/projectController');
 const passport = require('../middleware/auth');
 const createRateLimiter = require('../middleware/rateLimiter');
-const checkPermission = require('../middleware/checkPermission');
 
 
 
 const router = express.Router();
 const authenticateUser = passport.authenticate('jwt', { session: false });
 
-const checkPermissionsMiddleware = (requiredPermission) => async (req, res, next) => {
-    try {
-        const permissionGranted = await checkPermission(req, res, requiredPermission);
-        if (permissionGranted) {
-            next();
-        }
-    } catch (error) {
-        console.error('Permission check error:', error);
-        return res.status(500).json({ error: 'Internal server error' });
-    }
-};
 
-router.post('/create-project', authenticateUser, createRateLimiter(10 * 60 * 1000, 50), checkPermissionsMiddleware('create_project'), projectController.createProject);
-router.get('/get-all-project', authenticateUser, createRateLimiter(10 * 60 * 1000, 50), checkPermissionsMiddleware('get_all_project'), projectController.getAllProjects);
-router.patch('/update-project/:id', authenticateUser, createRateLimiter(10 * 60 * 1000, 50), checkPermissionsMiddleware('update_project'), projectController.updateProject);
-router.delete('/delete-project/:id', authenticateUser, createRateLimiter(10 * 60 * 1000, 50), checkPermissionsMiddleware('delete_project'), projectController.deleteProject);
-router.get('/get-project-data/:id', authenticateUser, createRateLimiter(10 * 60 * 1000, 50), checkPermissionsMiddleware('get_project'), projectController.getProjectById);
+router.post('/create-project', authenticateUser, createRateLimiter(10 * 60 * 1000, 50), projectController.createProject);
+router.get('/get-all-project', authenticateUser, createRateLimiter(10 * 60 * 1000, 50), projectController.getAllProjects);
+router.patch('/update-project/:id', authenticateUser, createRateLimiter(10 * 60 * 1000, 50), projectController.updateProject);
+router.delete('/delete-project/:id', authenticateUser, createRateLimiter(10 * 60 * 1000, 50), projectController.deleteProject);
+router.get('/get-project-data/:id', authenticateUser, createRateLimiter(10 * 60 * 1000, 50), projectController.getProjectById);
 
 module.exports = router;
--- routes/taskRoute.js
+++ routes/taskRoute.js
@@ -4,29 +4,17 @@ const passport = require('../middleware/auth');
 const fileHelper = require('../helper/fileHelper');
 const multer = require('multer');
 const createRateLimiter = require('../middleware/rateLimiter');
-const checkPermission = require('../middleware/checkPermission');
 
 
 const router = express.Router();
 const upload = multer();
 const authenticateUser = passport.authenticate('jwt', { session: false });
 
-const checkPermissionsMiddleware = (requiredPermission) => async (req, res, next) => {
-    try {
-        const permissionGranted = await checkPermission(req, res, requiredPermission);
-        if (permissionGranted) {
-            next();
-        }
-    } catch (error) {
-        console.error('Permission check error:', error);
-        return res.status(500).json({ error: 'Internal server error' });
-    }
-};
 
-router.post('/create-task/:id', authenticateUser, createRateLimiter(10 * 60 * 1000, 50), fileHelper.upload.single('File'), checkPermissionsMiddleware('create_task'), taskController.createTask);
-router.patch('/update-task/:id', authenticateUser, createRateLimiter(10 * 60 * 1000, 50), fileHelper.upload.single('File'), checkPermissionsMiddleware('update_task'), taskController.updateTask);
-router.delete('/delete-task/:id', authenticateUser, createRateLimiter(10 * 60 * 1000, 50), checkPermissionsMiddleware('delete_task'), taskController.deleteTask);
-router.get('/task-pagination/:id', authenticateUser, createRateLimiter(10 * 60 * 1000, 50), checkPermissionsMiddleware('task_pagination'), taskController.taskPagination);
-router.get('/get-task/:id', authenticateUser, createRateLimiter(10 * 60 * 1000, 50), checkPermissionsMiddleware('get_task'), taskController.getTask);
+router.post('/create-task/:id', authenticateUser, createRateLimiter(10 * 60 * 1000, 50), fileHelper.upload.single('File'), taskController.createTask);
+router.patch('/update-task/:id', authenticateUser, createRateLimiter(10 * 60 * 1000, 50), fileHelper.upload.single('File'), taskController.updateTask);
+router.delete('/delete-task/:id', authenticateUser, createRateLimiter(10 * 60 * 1000, 50), taskController.deleteTask);
+router.get('/task-pagination/:id', authenticateUser, createRateLimiter(10 * 60 * 1000, 50), taskController.taskPagination);
+router.get('/get-task/:id', authenticateUser, createRateLimiter(10 * 60 * 1000, 50), taskController.getTask);
 
 module.exports = router;
