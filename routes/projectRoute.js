dc50363fe4b88e41b2d7cd779a8a0c05fd0080c2
Bala1696 <balak1696@gmail.com>
2024-07-26 12:46:12 +0530

    Task Rote

--- routes/projectRoute.js
+++ routes/projectRoute.js
@@ -10,10 +10,10 @@ const router = express.Router();
 const authenticateUser = passport.authenticate('jwt', { session: false });
 
 
-router.post('/create-project', authenticateUser, sessionStatus, createRateLimiter(10 * 60 * 1000, 50),checkPermission('Projects', 'createProject'), projectController.createProject);
-router.get('/get-all-project', authenticateUser, sessionStatus, createRateLimiter(10 * 60 * 1000, 50),checkPermission('Projects', 'getAllProject'), projectController.getAllProjects);
-router.patch('/update-project/:id', authenticateUser, sessionStatus, createRateLimiter(10 * 60 * 1000, 50),checkPermission('Projects', 'updateProject'), projectController.updateProject);
-router.delete('/delete-project/:id', authenticateUser, sessionStatus, createRateLimiter(10 * 60 * 1000, 50), checkPermission('Projects', 'deleteProject'),projectController.deleteProject);
+router.post('/create-project', authenticateUser, sessionStatus, createRateLimiter(10 * 60 * 1000, 50), projectController.createProject);
+router.get('/get-all-project', authenticateUser, sessionStatus, createRateLimiter(10 * 60 * 1000, 50), projectController.getAllProjects);
+router.patch('/update-project/:id', authenticateUser, sessionStatus, createRateLimiter(10 * 60 * 1000, 50), projectController.updateProject);
+router.delete('/delete-project/:id', authenticateUser, sessionStatus, createRateLimiter(10 * 60 * 1000, 50), projectController.deleteProject);
 router.get('/get-project-data/:id', authenticateUser, sessionStatus, createRateLimiter(10 * 60 * 1000, 50),checkPermission('Projects', 'getProjectDataById'), projectController.getProjectDataById);
 
 module.exports = router;
